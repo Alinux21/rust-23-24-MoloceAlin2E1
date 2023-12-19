d03a90e07d321fd37f213c28ae4a76befdeb59a5
alinux <alinmoloce212@gmail.com>
2023-12-20 00:54:28 +0200

    Added Print only new posts every N seconds feature

--- redditor/target/debug/deps/redditor-cdf56f5311b44bfd.d
+++
@@ -1,5 +0,0 @@
-/home/alinux/Desktop/rust/rust-23-24-MoloceAlin2E1/redditor/target/debug/deps/redditor-cdf56f5311b44bfd.rmeta: src/main.rs
-
-/home/alinux/Desktop/rust/rust-23-24-MoloceAlin2E1/redditor/target/debug/deps/redditor-cdf56f5311b44bfd.d: src/main.rs
-
-src/main.rs:
