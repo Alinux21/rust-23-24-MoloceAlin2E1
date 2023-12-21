cd9fde0ea79153f0a76b4b4837f13b5a64d08ea4
alinux <alinmoloce212@gmail.com>
2023-12-21 23:07:59 +0200

    Successfully implemented the requirements from lab10. As a bonus if the cache memory is full and I enter a value from the cache it automatically move to the last position of the cache memory so it would be deleted later

--- lab10/target/debug/deps/lab10-3c04150c49c32e09.d
+++
@@ -1,5 +0,0 @@
-/home/alinux/Desktop/rust/rust-23-24-MoloceAlin2E1/lab10/target/debug/deps/lab10-3c04150c49c32e09.rmeta: src/main.rs
-
-/home/alinux/Desktop/rust/rust-23-24-MoloceAlin2E1/lab10/target/debug/deps/lab10-3c04150c49c32e09.d: src/main.rs
-
-src/main.rs:
--- lab10/target/debug/lab10.d
+++
@@ -1,1 +0,0 @@
-/home/alinux/Desktop/rust/rust-23-24-MoloceAlin2E1/lab10/target/debug/lab10: /home/alinux/Desktop/rust/rust-23-24-MoloceAlin2E1/lab10/src/main.rs
